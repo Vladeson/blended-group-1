62f01019f9b0001b4b3a3fad1d7317cda4118631
olgafrostpl <123272546+olgafrostpl@users.noreply.github.com>
2023-05-06 12:39:08 +0300

    Olga

--- js02.js
+++ js02.js
@@ -1,4 +1,3 @@
-
 // 1. У нас є об'єкт, в якому зберігатимуться зарплати
 // Нашої команди
 // Напишіть код для сумування всіх зарплат і
@@ -11,7 +10,6 @@
 // };
 
 // let sum = 0
-
 
 // for (const value of Object.values(salaries)) {
 //     sum += value
@@ -62,18 +60,97 @@
 // виведення buzz замість чисел, кратних 5;
 // виведення fizzbuzz замість чисел, кратних як 3, і 5.
 
-function fizzBuzz(num) {
-    for (let i = 1; i <= num; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log("fizzBuzz")
-        } else if (i % 3 === 0) {
-            console.log("fizz")
-        } else if (i % 5 === 0) {
-            console.log("buzz")
-        } else {
-            console.log(i)
-        }
-        
-    }
-}
-fizzBuzz(45)
+// function fizzBuzz(num) {
+//     for (let i = 1; i <= num; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log("fizzBuzz")
+//         } else if (i % 3 === 0) {
+//             console.log("fizz")
+//         } else if (i % 5 === 0) {
+//             console.log("buzz")
+//         } else {
+//             console.log(i)
+//         }
+
+//     }
+// }
+// fizzBuzz(45)
+
+//TODO:===========================
+// Створіть масив styles з елементами «Джаз» та «Блюз».
+//Додайте "Рок-н-рол" в кінець.
+// Замініть значення "Блюз" на "Класика".
+//Видалить перший елемент масиву і виведіть його в консоль.
+//Вставте «Реп» і «Реггі» на початок масиву.
+// const styles = ["jazz", "Bluz"];
+// styles.push("RocknRoll");
+// // styles[1] = "Classic";
+// styles.splice(1, 1, "Classic");
+// console.log(styles.slice(0, 1));
+// styles.unshift("Rep", "Reggy");
+// console.log(styles);
+
+//TODO:==========================
+//Напишіть функцію min(a, b), яка повертає
+//менше з чисел a, b
+//Потрібно додати перевірку, що функція отримує числа
+// function min(a, b) {
+//   if (Number.isInteger(a) && Number.isInteger(b)) {
+//     return Math.min(a, b);
+//   }
+//   return "`Один з аргументів не є числом`";
+// }
+// console.log(min(20, "ghhgjh"));
+
+//TODO:==============================
+// Напишіть функцію pow(x,n), яка повертає x до ступеня n.
+// Інакше висловлюючись, множить x він n разів і повертає результат.
+// function pow(x, n) {
+//   return console.log(Math.pow(x, n));
+// }
+// pow(2, 3);
+
+// 6. Пошук голосних
+
+// Потрібно написати функцію, яка приймає рядок як аргумент і повертає кількість
+// голосних, які містяться в рядку.
+// Голосними є "a", "e", "i", "o", "u".
+// function letters(string = "") {
+//   let counter = 0;
+
+//   const vowels = ["a", "e", "i", "o", "u"];
+//   for (const char of string.toLowerCase()) {
+//     if (vowels.includes(char)) {
+//       counter += 1;
+//     }
+//   }
+//   return counter;
+// }
+// console.log(letters("oiayurtgjkjooouuynlj"));
+
+//Напишіть ф-цію calcTotalPrice(stones, stonesName),
+//яка приймає масив об'єктів та
+//рядок під назвою каменю.
+//Функція  повертає загальну вартість каменів
+//з таким ім'ям, ціною та кількістю з об'єкта
+// const stones = [
+//   { name: "Изумруд", price: 1300, quantity: 4 },
+//   { name: "Бриллиант", price: 2700, quantity: 6 },
+//   { name: "Сапфир", price: 400, quantity: 7 },
+//   { name: "Щебень", price: 150, quantity: 100 },
+// ];
+// function calcTotalPrice(stones, stonesName) {
+//   for (const stone of stones) {
+//     const { name, price, quantity } = stone;
+//     if (stonesName === name) {
+//       return price * quantity;
+//     }
+//   }
+// }
+// function add(newName, newPrice = 10, newQuant = 20) {
+//   const newStone = { name: newName, price: newPrice, quantity: newQuant };
+//   stones.push(newStone);
+// }
+// console.log(calcTotalPrice(stones, "Изумруд"));
+// add("Diamond");
+// console.log(stones);
